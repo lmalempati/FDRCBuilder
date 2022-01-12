55adffe2e1c299018b314735711a176498a5ee92
Lakshmi Narayana Malempati <Lakshmi.malempati@thoughtfocus.com>
2022-01-12 10:48:48 +0530

    > Removed CardInfo.java, OrigAuthInfo.java

--- src/main/java/fdrc/base/OrigAuthInfo.java
+++
@@ -1,9 +0,0 @@
-package fdrc.base;
-
-public class OrigAuthInfo {
-    public String origAuthID;
-    public String origLocalDateTime;
-    public String origTranDateTime;
-    public String origRespCode;
-    public String origSTAN;
-}
