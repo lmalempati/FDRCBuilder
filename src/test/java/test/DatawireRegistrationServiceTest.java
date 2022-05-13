cf59607e8f3953fa86071cb92ce6ab659d5597d2
lakshmimalempati <lakshmi.malempati@thoughtfocus.com>
2022-05-13 17:14:36 +0530

    Updated tests and json test data files

--- src/test/java/test/DatawireRegistrationServiceTest.java
+++ src/test/java/test/DatawireRegistrationServiceTest.java
@@ -1,5 +1,8 @@
 package test;
 
+import fdrc.common.Serialization;
+import fdrc.model.DatawireSRSActivationResponse;
+import fdrc.model.DatawireSRSRegistrationResponse;
 import fdrc.model.DatawireSRSRequest;
 import fdrc.model.DatawireSRSResponse;
 import fdrc.service.Client;
@@ -18,8 +21,27 @@ class DatawireRegistrationServiceTest {
         // Already on boarded mid
         srsResponse = client.submitDatawireSRS( new DatawireSRSRequest(false, "RCTST1000094637", "3", "10001", "RSU005"));
         // "Registration request failed, status: Merchant Already Provisioned and statuscode: AccessDenied"
-//        boolean found = result.indexOf("Merchant Already Provisioned") >= 0;
+        boolean found = srsResponse.errorMsg.indexOf("Merchant Already Provisioned") >= 0;
         boolean found1 = srsResponse.errorMsg.indexOf("AccessDenied") >= 0;
-        assertEquals(found1, true);
+        assertEquals(found || found1, true);
+    }
+
+    @Test
+    public void testRegistrationRespSerilization(){
+        String response = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><!DOCTYPE Response PUBLIC \"-//Datawire Communication Networks INC//DTD VXN API Self-Registration 3.0//EN\" \"http://www.datawire.net/xmldtd/srs.dtd\"><Response Version=\"3\"><RespClientID><DID>00043816825221507852</DID><ClientRef>0164704VRSU005</ClientRef></RespClientID><Status StatusCode=\"OK\"/><RegistrationResponse><DID>00043816825221507852</DID><URL>https://stg.dw.us.fdcnet.biz/rc</URL><URL>https://stg.dw.us.fdcnet.biz/rc</URL></RegistrationResponse></Response>\n" +
+                "\n";
+        DatawireSRSRegistrationResponse regResp = (DatawireSRSRegistrationResponse) Serialization.getObjectFromXML(DatawireSRSRegistrationResponse.class, response, false);
+        assertEquals("00043816825221507852", regResp.RegistrationResponse.DID);
+        assertEquals("OK", regResp.Status.StatusCode);
+    }
+
+    @Test
+    public void testactivationRespSerilization() {
+        String actResp = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><!DOCTYPE Response PUBLIC \"-//Datawire Communication Networks INC//DTD VXN API Self-Registration 3.0//EN\" \"http://www.datawire.net/xmldtd/srs.dtd\"><Response Version=\"3\"><RespClientID><DID/><ClientRef>935406V1RSU005</ClientRef></RespClientID><Status StatusCode=\"OK\"/><ActivationResponse/></Response>";
+
+        DatawireSRSActivationResponse datawireSRSActivationResponse = (DatawireSRSActivationResponse) Serialization.getObjectFromXML(DatawireSRSActivationResponse.class, actResp, false);
+        assertEquals("935406V1RSU005", datawireSRSActivationResponse.RespClientID.ClientRef);
+        assertEquals("OK", datawireSRSActivationResponse.Status.StatusCode);
+
     }
 }
--- src/test/java/test/TestsMoto.java
+++ src/test/java/test/TestsMoto.java
@@ -8,10 +8,10 @@ import static org.junit.Assert.assertEquals;
 public class TestsMoto extends TestGeneric {
     public static final String PATH = "src/test/resources/Moto/";
 
-//    @ParameterizedTest
+    @ParameterizedTest
     @ValueSource(strings =
             {
-                    "Amex-AuthCompl-payload-10.json;002;1"
+//                    "Amex-AuthCompl-payload-10.json;002;1",
 //                    , "Amex-AuthCompl-payload-11.json;000", // Auth & Compl Amex
 //                    "JCB-AuthCompl-Keyed-100070950010.json;000;1", "JCB-AuthCompl-Keyed-100070950011.json;000",
 //                    "DS-Auth-Keyed-200070910010.json;000",
@@ -23,7 +23,7 @@ public class TestsMoto extends TestGeneric {
 //                    "Diners-AuthCompl-200070930010.json;000;1", "Diners-AuthCompl-200070930011.json;000",
 //                    "Amex-AuthCompl-201013680010.json;002;1", "Amex-AuthCompl-201013680011.json;000",
 //                    "credit-AuthVoid-200070840010.json;000;1", "credit-AuthVoid-200070840011.json;000", // Auth & void..
-//                    "AddtlAmount-Credit-MC-Sale-200004170020.json;500", // Sale
+                    "MC-AddtlAmount-Credit-Sale-200004170020.json;500", // Sale
 //                    "Credit-TA-Auth-200021900010.json;000;1", "Credit-TA-Auth-200021900011.json;000",
 //                    "TATokenRequest-200004710010.json;000",
 //                    "Credit-TA-Refund-Disc-200292180010.json;000",
